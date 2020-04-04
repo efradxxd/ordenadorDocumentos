0030a433d1a24d272910c91587fd72d795c3322e
efradxxd <efradxxd@MacBook-Pro.lan>
2020-04-04 03:22:52 -0600

    add comments

--- archivos.py
+++ archivos.py
@@ -6,6 +6,7 @@ salirPrograma = False
 
 while salirPrograma == False:
     print(">>Bienvenido estos archivos existen en el directorio")
+    #se llen los archivos existentes el directorio indicado
     for dirpath, dirnames, filenames in os.walk("./archivos"):
         print("Ruta actual:", dirpath)
         print("Carpetas:", ", ".join(dirnames))
@@ -17,20 +18,21 @@ while salirPrograma == False:
         print(">>La ruta que selecionaste no existe, Teclea una ruta valida")
         nombreArchivo = input()
         rutaArchivo = "./archivos/" + nombreArchivo
-    # print(">>Nombre Archivo: ",nombreArchivo)
-    # print(">>Tamaño Archivo: ", os.path.getsize(nombreArchivo)," Bytes")
+    #Se obtiene el tamano del archivo en bytes
     tamano = os.path.getsize(rutaArchivo);
+    #Se obtiene la codificacion de archivo con apoyo de la libreria chardet
     rawdata = open(rutaArchivo, "rb").read()
     result = chardet.detect(rawdata)
     codificacionArchivo = result['encoding']
-    # print(">>Codificación Archivo:")
-    # print(codificacionArchivo)
+    #Se crea objeto con las propiedades definidas en la clase documento
     objetoDocumento = doc(nombreArchivo,tamano,codificacionArchivo)
+    #Se agrega objeto a arreglo
     arregloDocumentos.append(objetoDocumento)
     print(">>Documentos Sin Ordenar ")
     for i in arregloDocumentos:
         print (">> Nombre: ",i.nombre," Tamaño: ",i.tamano,"Bytes ","Codificacón: "+i.codificacion);
     print(">>Documentos Ordenados en base a su Tamaño")
+    #Se ordena areglo de documento en base a la propiedad tamano
     arregloDocumentosOrdenado = sorted(arregloDocumentos, key=lambda objeto: objeto.tamano)
     for i in arregloDocumentosOrdenado:
         print (">> Nombre: ",i.nombre," Tamaño: ",i.tamano,"Bytes ","Codificacón: "+i.codificacion)
